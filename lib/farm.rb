613aecf8d9ab93dd393d9af85d409d94360fb018
Matt Manning <matt.manning@gmail.com>
2012-01-20 02:27:47 -0500

    Fix typo

--- lib/farm.rb
+++ lib/farm.rb
@@ -2,5 +2,5 @@ require 'active_support'
 require 'farm/message_sending'
 require 'farm/performable_method'
 
-Object.send(:indlude, Farm::MessageSending)
+Object.send(:include, Farm::MessageSending)
 Module.send(:include, Farm::MessageSending::ClassMethods)
