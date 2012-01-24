c4905cc883fa75fdb73a0a0cd85b78dc8657879c
Matt Manning <matt.manning@gmail.com>
2012-01-23 22:50:38 -0500

    Implement FARM_ENV. Don't memoize @payload_object.

--- lib/farm/runner.rb
+++ lib/farm/runner.rb
@@ -5,13 +5,21 @@ require 'yaml'
 module Farm
   class Runner
     def self.run(*args)
-      @payload_object ||= args.shift[:payload_object]
+      @payload_object = args.shift[:payload_object]
 
       unless @payload_object.respond_to?(:perform)
         raise ArgumentError, 'Cannot run items which do not respond to perform'
       end
 
-      Farm::Http.heroku_run CGI.escape(YAML.dump(@payload_object))
+      if ENV['FARM_ENV'].to_s.strip == 'local'
+        `rake farm:run CMD='#{serialized}'`
+      else
+        Farm::Http.heroku_run serialized
+      end
+    end
+
+    def self.serialized
+      CGI.escape(YAML.dump(@payload_object))
     end
   end
 end
