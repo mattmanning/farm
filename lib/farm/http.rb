37883b68d00c5f31023c1c7dc14128ab6560ddfa
Matt Manning <matt.manning@gmail.com>
2012-01-24 21:31:15 -0500

    Parse API JSON response and return it as a Ruby Hash.

--- lib/farm/http.rb
+++ lib/farm/http.rb
@@ -1,12 +1,15 @@
+require 'json'
 require 'rest_client'
 
 module Farm
   class Http
     def self.heroku_run(cmd)
-      endpoint = RestClient::Resource.new "https://api.heroku.com/apps/#{ENV['APP_NAME']}/ps",
-        '', ENV['API_KEY']
-      endpoint.post({ :command => 'rake farm:run', :ps_env => { 'CMD' => cmd } },
-                      :accept => 'application/json')
+      endpoint    = RestClient::Resource.new "https://api.heroku.com/apps/#{ENV['APP_NAME']}/ps",
+                    '', ENV['API_KEY']
+      headers     = { :accept => 'application/json' }
+      post_params = { :command => 'rake farm:run', :ps_env => { 'CMD' => cmd } }
+      response    = endpoint.post post_params, headers
+      JSON.parse response
     end
   end
 end
