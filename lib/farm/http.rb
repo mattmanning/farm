5d7615dabf33f044b29245ed8deb8bf141e7db8c
Matt Manning <matt.manning@gmail.com>
2012-01-24 21:12:22 -0500

    Pass accept header correctly so we get a JSON response.

--- lib/farm/http.rb
+++ lib/farm/http.rb
@@ -5,7 +5,8 @@ module Farm
     def self.heroku_run(cmd)
       endpoint = RestClient::Resource.new "https://api.heroku.com/apps/#{ENV['APP_NAME']}/ps",
         '', ENV['API_KEY']
-      endpoint.post :command => 'rake farm:run', :ps_env => { 'CMD' => cmd }, :accept => 'application/json'
+      endpoint.post({ :command => 'rake farm:run', :ps_env => { 'CMD' => cmd } },
+                      :accept => 'application/json')
     end
   end
 end
